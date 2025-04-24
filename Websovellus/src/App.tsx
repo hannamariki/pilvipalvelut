b7df8e99e79712646f9079e4e4d0befa208ec93a
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2025-04-24 19:15:46 +0300

    Lisätty ProductList ja ForeStore

--- Websovellus/src/App.tsx
+++ Websovellus/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LoginForm from './LoginForm';
+import ProductList from './ProductList';
 
 function App() {
 //Asetetaan cookie
@@ -53,10 +54,14 @@ function App() {
         <div>
           <LoginForm />
         </div>
+        <div>
+          <ProductList />
+        </div>
         <p className="read-the-docs">
           Click on the Vite and React logos to learn more
         </p>
       </div> {/* ← tämä lisättiin */}
+      <div></div>
     </>
   )
 }
