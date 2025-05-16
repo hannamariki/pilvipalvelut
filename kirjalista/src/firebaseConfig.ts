b552905cc0931f11e4f79f6e6d0a83923a1cb18f
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2025-05-16 13:34:52 +0300

    Kirjalistan takaisin lisäys

---
+++ kirjalista/src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDc0wcpgNpTfIgHwsY8ZNRwcFu3UYgtkUk",
+  authDomain: "kirjalista-422ca.firebaseapp.com",
+  projectId: "kirjalista-422ca",
+  storageBucket: "kirjalista-422ca.firebasestorage.app",
+  messagingSenderId: "510040733332",
+  appId: "1:510040733332:web:1153248ef147401eb0e681"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
